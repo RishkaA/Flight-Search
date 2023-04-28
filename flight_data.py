fe8536267743c1c9e9d3b096fb9a9695c0464656
RishkaA <77984459+RishkaA@users.noreply.github.com>
2023-04-28 13:11:07 +0100

    Update flight_data.py
    added stopover attributes

--- flight_data.py
+++ flight_data.py
@@ -1,10 +1,16 @@
 class FlightData:
-
-    def __init__(self, price, departure_airport, departure_city, to_airport, to_city, from_date, to_date):
-       self.price = price,
-       self.departure_airport = departure_airport,
-       self.departure_city = departure_city,
-       self.to_airport = to_airport,
-       self.to_city = to_city,
-       self.from_date = from_date,
-       self.to_date = to_date
+  '''
+  class stores iformation of one flight found in the search
+  '''
+  def __init__(self, price, departure_airport, departure_city, to_airport, to_city, from_date, to_date, outbound_stopovers=0, back_stopovers=0,  outbound_via_city="", back_via_city=""):
+     self.price = price,
+     self.departure_airport = departure_airport,
+     self.departure_city = departure_city,
+     self.to_airport = to_airport,
+     self.to_city = to_city,
+     self.from_date = from_date,
+     self.to_date = to_date
+     self.outbound_stopovers = outbound_stopovers,
+     self.back_stopovers = back_stopovers,
+     self.outbound_via_city = outbound_via_city,
+     self.back_via_city = back_via_city
